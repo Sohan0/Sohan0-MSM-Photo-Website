ef9934674d7131a7ea9489b0e745640c235e391d
Sohan0 <villenvillen726@gmail.com>
2022-08-14 21:28:29 +0600

    hello

---
+++ script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    var typed = new Typed('.element', {
+        strings: ["Sohan Sarkar.", "a graphic designer", "Photographer"],
+        typeSpeed: 100,
+        backSpeed:50,
+        loop:true,
+        startDelay: 500,
+        showCursor: false,
+      });
+      var mixer = mixitup('#mix-it-up');
+})
